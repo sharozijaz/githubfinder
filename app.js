47ae8362ac279a7dedcd8feba52b6e4e3a6947c3
“Sharoz <shahrozijaz2@gmail.com>
2020-10-10 12:39:40 +0500

    Project Completed

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-// init github
-const github = new GitHub;
+// Init Github
+const github = new Github;
 // Init UI
 const ui = new UI;
-// Search Input
+
+// Search input
 const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-    // Get Input text
-    const userText = e.target.value;
+  // Get input text
+  const userText = e.target.value;
 
-    if (userText !== ''){
-        // Make http call
-        github.getUser(userText).then(data => {
-            if(data.profile.message === 'Not Found'){
-                // Show Alert
-
-            }
-            else{
-                // Show Profile
-                ui.showProfile(data.profile);
-            }
-        })
-    }
-    else{
-        // clear profile
-    }
-
-});
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
